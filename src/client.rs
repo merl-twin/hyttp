63f15dd58d31ff91d8d82e955cedb79c5aa8579f
merl <merl@kribrum.ru>
2021-05-13 15:02:14 +0300

    request timeout

--- src/client.rs
+++ src/client.rs
@@ -13,6 +13,7 @@ pub enum ClientError {
     Uri(hyper::http::uri::InvalidUri),
     Request(hyper::http::Error),
     Hyper(hyper::Error),
+    Timeout(String),
 }
 
 pub struct ClientRequest {
@@ -54,6 +55,7 @@ impl ClientRequest {
             http_conn.set_connect_timeout(self.connect_timeout);
             http_conn
         });
+        let tm_uri = self.server_uri.to_string();
         let req = match self.json {
             Some(json) => Request::post(self.server_uri)
                 .header(CONTENT_TYPE,mime::APPLICATION_JSON.as_ref())
@@ -63,7 +65,12 @@ impl ClientRequest {
         }.map_err(ClientError::Request)?;
 
 
-        let response = client.request(req).await.map_err(ClientError::Hyper)?;       
+        let response = match self.request_timeout {
+            None => client.request(req).await.map_err(ClientError::Hyper)?,
+            Some(tm) => tokio::time::timeout(tm,client.request(req)).await
+                .map_err(move |_| ClientError::Timeout(tm_uri))?
+                .map_err(ClientError::Hyper)?,
+        };
         let buffer = body::to_bytes(response.into_body()).await.map_err(ClientError::Hyper)?;
 
         serde_json::from_slice(buffer.as_ref()).map_err(ClientError::Json)
@@ -77,25 +84,33 @@ impl ClientRequest {
 pub struct Client<D: DeserializeOwned> {
     cli: client::Client<client::HttpConnector>,
     _d: std::marker::PhantomData<D>,
+    request_timeout: Option<std::time::Duration>, 
 }
 impl<D: DeserializeOwned> Client<D> {
     pub fn new() -> Client<D> {
         Client {
             cli: client::Client::new(),
             _d: std::marker::PhantomData,
+            request_timeout: None, 
         }
     }
-    pub fn with_connect_timeout(timeout: std::time::Duration) -> Client<D> {
+    pub fn with_connect_timeout(self, timeout: std::time::Duration) -> Client<D> {
         Client {
             cli: client::Builder::default().build({
                 let mut http_conn = HttpConnector::new();
                 http_conn.set_connect_timeout(Some(timeout));
                 http_conn
             }),
+            request_timeout: None, 
             _d: std::marker::PhantomData,
         }
     }
+    pub fn with_request_timeout(mut self, tm: std::time::Duration) -> Client<D> {
+        self.request_timeout = Some(tm);
+        self
+    }
     pub async fn post_json<S: Serialize>(&self, uri: &str, req: &S) -> Result<D,ClientError> {
+        let tm_uri = uri.to_string();
         let uri: Uri = uri.parse().map_err(ClientError::Uri)?;
         let json = serde_json::to_string(req).map_err(ClientError::Json)?.into_bytes();
         let req = Request::post(uri)
@@ -103,16 +118,27 @@ impl<D: DeserializeOwned> Client<D> {
             .header(CONTENT_LENGTH,json.len() as u64)
             .body(Body::from(json))
             .map_err(ClientError::Request)?;
-        let response = self.cli.request(req).await.map_err(ClientError::Hyper)?;       
+        let response = match self.request_timeout {
+            None => self.cli.request(req).await.map_err(ClientError::Hyper)?,
+            Some(tm) => tokio::time::timeout(tm,self.cli.request(req)).await
+                .map_err(move |_| ClientError::Timeout(tm_uri))?
+                .map_err(ClientError::Hyper)?,
+        };
         let buffer = body::to_bytes(response.into_body()).await.map_err(ClientError::Hyper)?;
         serde_json::from_slice(buffer.as_ref()).map_err(ClientError::Json)
     }
     pub async fn get(&self, uri: &str) -> Result<D,ClientError> {
+        let tm_uri = uri.to_string();
         let uri: Uri = uri.parse().map_err(ClientError::Uri)?;
         let req = Request::get(uri)
             .body(Body::empty())
             .map_err(ClientError::Request)?;
-        let response = self.cli.request(req).await.map_err(ClientError::Hyper)?;       
+        let response = match self.request_timeout {
+            None => self.cli.request(req).await.map_err(ClientError::Hyper)?,
+            Some(tm) => tokio::time::timeout(tm,self.cli.request(req)).await
+                .map_err(move |_| ClientError::Timeout(tm_uri))?
+                .map_err(ClientError::Hyper)?,
+        };
         let buffer = body::to_bytes(response.into_body()).await.map_err(ClientError::Hyper)?;
         serde_json::from_slice(buffer.as_ref()).map_err(ClientError::Json)
     }
